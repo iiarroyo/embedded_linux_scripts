5a8e42f680482a035bda004624ad1d38275e18cf
israel-iap <israel.iap@outlook.com>
2021-04-20 20:43:49 -0500

    test

--- boton_mensaje.py
+++ boton_mensaje.py
@@ -8,11 +8,11 @@ while bandera: #ejecuta instruccion hasta interrupcion
     try:
         button.wait_for_press()# esperar presionado
         print("Botón presionado") 
+        led.on()
         #prender led despues de presionado
         #imprimir mensaje en consola
 
-        button.wait_for_press()# esperar presionado
-        led.on()
+        button.wait_for_release()# esperar presionado
         led.off()
     except KeyboardInterrupt:
         print("Programa finalizado")
