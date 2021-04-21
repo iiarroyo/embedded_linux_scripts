658d31c6bea95a9d10888af346c54e107da1df5b
israel-iap <israel.iap@outlook.com>
2021-04-21 12:11:38 -0500

    morse final version

--- morse1.py
+++ morse1.py
@@ -1,47 +1,47 @@
-#from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 
-#led = LED(25)
+led = LED(25)
 
 def morse(n):
     if(n == 0):
        for i in range (5):
             led.on()
-            print(".")
-            sleep(1)
+            print("_")
+            sleep(2)
             led.off()
             print(" ")
-            sleep(0.3)    
+            sleep(0.2)    
     elif(n<=5):
         for i in range (n):
             led.on()
             print(".")
-            sleep(1)
+            sleep(0.5)
             led.off()
             print(" ")
-            sleep(0.3)          
+            sleep(0.2)          
         for i in range (5-n):
+            led.on()
+            print("_")
+            sleep(2)
             led.off()
             print(" ")
-            sleep(1)
-            led.off()
-            print(" ")
-            sleep(0.3) 
+            sleep(0.2) 
     else:      
         for i in range (n-5):
+            led.on()
+            print("_")
+            sleep(2)
             led.off()
             print(" ")
-            sleep(1)
-            led.off()
-            print(" ")
-            sleep(0.3)
+            sleep(0.2)
         for i in range (10-n):
             led.on()
             print(".")
-            sleep(1)
+            sleep(0.5)
             led.off()
             print(" ")
-            sleep(0.3) 
+            sleep(0.2) 
 
 num = int(input())
 morse(num)
